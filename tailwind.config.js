e858dd8cf3a7fae6b9a63942437beb0705affdd1
yesiddev <yesiddeveloper@gmail.com>
2023-06-01 18:07:51 -0500

    config tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.html"],
+  theme: {
+    colors: {
+      transparent: "transparent",
+      dark: "#11111b",
+      white: "#ffffff",
+      light: "#d7dae0",
+      background: "#1e1e2e",
+      purple: "#cba6f7",
+      green: "#a6e3a1",
+      orange: "#fab387",
+      red: "#f38ba8",
+    },
+  },
+  plugins: [],
+};
