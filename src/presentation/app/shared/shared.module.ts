545e7cbb4d0710412ef7e7f79e37c03da000569a
yesiddev <yesiddeveloper@gmail.com>
2023-06-06 10:25:53 -0500

    create sidebar

--- src/presentation/app/shared/shared.module.ts
+++ src/presentation/app/shared/shared.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { SidebarComponent } from './sidebar/sidebar.component';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
 import { RouterModule } from '@angular/router';
 
 @NgModule({
